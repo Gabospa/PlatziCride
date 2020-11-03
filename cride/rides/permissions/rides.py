c386a8e442cc9c6eee6bed615a473d24d97d343b
Gabriel Ospina <gabospa@gmail.com>
2020-11-02 23:47:03 -0500

    Add rides and methods for joining and finish

---
+++ cride/rides/permissions/rides.py
@@ -0,0 +1,21 @@
+"""Rides Permissions."""
+
+
+# Django REST Framework
+from rest_framework.permissions import BasePermission
+
+
+class IsRideOwner(BasePermission):
+    """Verify requesting user is the ride creator."""
+
+    def has_object_permission(self, request, view, obj):
+        """Verify requesting user is the ride creator."""
+        return request.user == obj.offered_by
+
+class IsNotRideOwner(BasePermission):
+    """Verify requesting user is not the owner of ride."""
+
+    def has_object_permission(self, request, view, obj):
+        """Verify requesting user is not the owner of ride."""
+        return request.user != obj.offered_by
+
